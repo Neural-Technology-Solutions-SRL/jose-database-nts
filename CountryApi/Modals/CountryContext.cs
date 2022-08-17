a7153417c997e79b69a5b1ddef5449da45488e95
jmontero-nts <jmontero@neural.com.do>
2022-08-17 18:03:10 -0400

    country Api

---
+++ CountryApi/Modals/CountryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CountryApi.Modals
+{
+    public class CountryContext:DbContext
+    {
+        public CountryContext(DbContextOptions<CountryContext> options):base(options)
+        {
+        }
+            
+            
+            public DbSet<CountryItem> CountryItems { get; set; } = null!;
+    }
+    
+}
