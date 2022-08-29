d3219212844ada57e97f7c28fe654cade35f0e1b
jose montero <jose142578@gmail.com>
2022-08-29 02:38:16 -0400

    backend published

--- CountryApi/Repository/CountriesDbContext.cs
+++ CountryApi/Repository/CountriesDbContext.cs
@@ -24,7 +24,7 @@ namespace CountryApi.Repository
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-0SQ0GC1\\SQLEXPRESS;Initial Catalog=Countries;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=DBTODOAPI.mssql.somee.com;Initial Catalog=DBTODOAPI;user id=Joseneural_SQLLogin_1;pwd=ua1769bcby");
             }
         }
 
